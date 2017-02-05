36e90097daf8623df2fcce9819b548d6f8824846
Oliver Keyes <ironholds@gmail.com>
2017-02-05 07:44:55 +0000

    version 1.5.0

--- R/WikipediR.R
+++ R/WikipediR.R
@@ -2,9 +2,9 @@
 #' @name WikipediR
 #' @description This package provides functions for accessing the MediaWiki API, either for
 #' Wikimedia projects or any other MediaWiki instance. For more information, see the
-#' \href{https://cran.r-project.org/web/packages/WikipediR/vignettes/WikipediR.html}{vignette}.
+#' \href{https://CRAN.R-project.org/package=WikipediR/vignettes/WikipediR.html}{vignette}.
 #' 
-#' @seealso The \href{https://cran.r-project.org/web/packages/WikipediR/vignettes/WikipediR.html}{package vignette}.
+#' @seealso The \href{https://CRAN.R-project.org/package=WikipediR/vignettes/WikipediR.html}{package vignette}.
 #' @docType package
 #' @importFrom httr GET user_agent stop_for_status
 #' @importFrom utils URLencode
